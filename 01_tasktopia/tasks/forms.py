d2181e3afdc0f3b59f5176ef73935e7f7872a400
IGWE, Chibueze C. <scientistigwe@gmail.com>
2024-06-20 00:56:56 +0100

    modify task _list.html

--- 01_tasktopia/tasks/forms.py
+++ 01_tasktopia/tasks/forms.py
@@ -1,16 +1,17 @@
-# tasks/forms.py
-
 from django import forms
 from .models import Task, Category
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description', 'due_date', 'priority', 'status', 'category', 'user']
+        fields = ['title', 'description', 'due_date', 'priority', 'status', 'category']
         widgets = {
-            'due_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-            'priority': forms.Select(),
-            'status': forms.Select(),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'due_date': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+            'priority': forms.Select(attrs={'class': 'form-select'}),
+            'status': forms.Select(attrs={'class': 'form-select'}),
+            'category': forms.Select(attrs={'class': 'form-select'}),
         }
 
 class CategoryForm(forms.ModelForm):
