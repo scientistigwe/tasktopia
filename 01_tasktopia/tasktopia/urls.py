26fa4db18b83e44c1c834f75793ba49c6930cbab
IGWE, Chibueze C. <scientistigwe@gmail.com>
2024-06-20 13:14:44 +0100

    add landing page html to handle login and account management

--- 01_tasktopia/tasktopia/urls.py
+++ 01_tasktopia/tasktopia/urls.py
@@ -2,11 +2,15 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 #from dashboard.views import dashboard_view 
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', admin.site.urls),
-    path('reports/', include('reports.urls')),
+    #path('accounts/', include('django.contrib.auth.urls')),
+    #path('reports/', include('reports.urls')),
     path('tasks/', include('tasks.urls')),  # Updated path for tasks
     #path('dashboard/', dashboard_view, name='dashboard'),
+
 ]
