cb96e7ff1b42fcc06e2325c95d2788d9b9683c34
IGWE, Chibueze C. <scientistigwe@gmail.com>
2024-06-22 16:25:30 +0100

    styling of task.html files part 1.

--- 01_tasktopia/tasks/views.py
+++ 01_tasktopia/tasks/views.py
@@ -11,6 +11,11 @@ class TaskListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user  # Add user to context
+        return context
 
 class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
