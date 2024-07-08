57011201b3b184c7125024acce755207b2c644e2
IGWE, Chibueze C. <scientistigwe@gmail.com>
2024-07-08 09:06:13 +0100

    attempt deployment

--- tasktopia/settings.py
+++ tasktopia/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-mqnrowkrk6%ayjkbr_q0gf(%yx_vx4-!(se@9y1besan2-jgsf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*',
